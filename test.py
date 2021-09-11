8edf0c743370d932bb14132840d32c9bc2140f3a
Sasan Sohrabi <63074497+sasan-sohrabi@users.noreply.github.com>
2021-09-11 20:32:18 +0430

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from kavenegar import *
 
-api = KavenegarAPI('39784B664E4442664D64717676696C566E763857316A556D39436D6C785251616F4C4F55367351694375513D')
-params = {'sender': '10000400600600', 'receptor': '09125915669', 'message': 'صرفا جهت تست'}
+api = KavenegarAPI('*')
+params = {'sender': '*', 'receptor': '*', 'message': 'صرفا جهت تست'}
 response = api.sms_send(params)
